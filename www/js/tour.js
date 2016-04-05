187a666c870bc1e6969668337c6b212ba419326d
Amin <marashi.s.amin@gmail.com>
2016-04-05 08:18:07 +0430

    Saving progress

--- www/js/tour.js
+++ www/js/tour.js
@@ -3,54 +3,38 @@ var startIntro = function startIntro(){
 	intro.setOptions({
 		showStepNumbers: false,
 		steps: [
+			{
+				element: $(".blocklyBlockCanvas:contains('Submarket')")[0],
+				intro: 'This is the trade block.',
+				position: 'bottom',
+			},
 			{ 
 				intro: 'Welcome to the binary-bot!',
 				showButtons: false,
 			},
 			{
-				element: '#placeHolderForWorkspace',
 				intro: 'This is the main workspace, you can add blocks to define your functionality here.',
+				highlightClass: 'hidden',
+			},
+			{
+				element: '.intro_xml_files',
+				intro: 'Open/Save your blocks using these tools.',
+			},
+			{
+				element: '.intro_token',
+				intro: 'Add a token to use in the bot. At least one token has to be added.',
+			},
+			{
+				element: '.intro_code',
+				intro: 'Run the code when ready. Stop execution of the code.',
+			},
+			{
+				element: '#placeHolderForTrash',
+				intro: 'Remove the blocks by dropping them in here.',
 				position: 'left',
 				highlightClass: 'hidden',
 			},
-		],
-	});
-	intro.oncomplete(function(){
-		var intro2 = introJs();
-		intro2.setOptions({
-			showStepNumbers: false,
-			showButtons: false,
-			steps: [
-				{ 
-					intro: 'Welcome to the binary-bot!',
-					showButtons: false,
-				},
-				{
-					element: '.blocklyToolboxDiv',
-					intro: 'Pick blocks from here.',
-					position: 'right',
-				},
-				{
-					element: '.intro_xml_files',
-					intro: 'Open/Save your blocks using these tools.',
-				},
-				{
-					element: '.intro_token',
-					intro: 'Add a token to use in the bot. At least one token has to be added.',
-				},
-				{
-					element: '.intro_code',
-					intro: 'Run the code when ready. Stop execution of the code.',
-				},
-				{
-					element: '#placeHolderForTrash',
-					intro: 'Remove the blocks by dropping them in here.',
-					position: 'left',
-					highlightClass: 'hidden',
-				},
-			]
-		});
-		intro2.start();
+		]
 	});
 	intro.start();
 };
